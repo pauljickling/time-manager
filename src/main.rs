f8a1a764f432cb16af81978791915b62c2a3c904
Paul Jickling <pjickling@gmail.com>
2019-01-22 16:51:08 -0800

    Adjusted struct

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ use chrono::prelude::*;
 
 fn main() {
     let local: DateTime<Local> = Local::now();
-    let mut start = String::from("this is a start");
+    let mut name = String::from("this is a name");
     let mut summary = String::from("this is a summary");
     let activity = Activity {start, summary, local};
 
-    println!("{} with the time {:?}", activity.start, activity.local);
+    println!("{} with the time {:?}", activity.name, activity.local);
     
 }
 
 struct Activity {
-    start: String,
+    name: String,
     summary: String,
     local: DateTime<Local>,
 }
