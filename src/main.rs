b0e3e1bfff9de6e19c8bcf6b855ee5b289a41798
Paul Jickling <pjickling@gmail.com>
2019-01-27 11:07:26 -0800

    Added error and fs from std lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::error::Error;
 use chrono::prelude::*;
 
 fn main() {
@@ -21,4 +23,10 @@ fn write_file(path: String, text: String) -> std::io::Result<()> {
  41     fs::write(path, text)?;
  42     Ok(())
  43 }
+
+ // read file contents and return Some or None
+ 36 fn read(path: &String) -> Result<String, Box<dyn Error>> {
+ 37     let contents = fs::read_to_string(path)?;
+ 38     Ok(contents)
+ 39 }
 
