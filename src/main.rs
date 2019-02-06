baa23164655cd3cb95f14ba161a40881135727d8
Paul Jickling <pjickling@gmail.com>
2019-02-06 06:36:12 -0800

    Added get_date function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::io;
 use std::fs;
 use std::error::Error;
+use std::process::Command;
 
 fn main() {
     let name = String::from("programming");
     let mut entries: Vec<String> = Vec::new();
     entries.push(String::from("start"));
+    entries.push(String::from(get_date()));
     entries.push(String::from("stop"));
+    entries.push(String::from(get_date()));
     let activity = Activity {name, entries};
 
     println!("{} has the following entries: {:?}", activity.name, activity.entries);
@@ -28,5 +31,20 @@ fn _write_file(path: String, text: String) -> std::io::Result<()> {
 fn read(path: &String) -> Result<String, Box<dyn Error>> {
     let contents = fs::read_to_string(path)?;
     Ok(contents)
-}
+    }
 
+// gets date string from terminal
+fn get_date() -> String {
+    let output = Command::new("date")
+                          .output()
+                          .expect("Failed to execute command");  
+
+    let date_seq = output.stdout.as_slice();
+    let mut date = String::new();
+    for c in date_seq {
+        if c.is_ascii() == true {
+            date.push(*c as char);
+        }
+    }
+    date
+}
