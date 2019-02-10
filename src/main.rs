643838177fe336649c467ec9d776c9bcbdea6d40
Paul Jickling <pjickling@gmail.com>
2019-02-10 09:07:41 -0800

    Added match expression for Action enum

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::fs;
 use std::error::Error;
 use std::process::Command;
 
-// TODO match expression that adds string to Record if valid variant, else err
 enum Action {
     Start,
     Stop,
@@ -25,14 +24,21 @@ fn main() {
     println!("Struct test");
     let activity = String::from("sample task");
     let action = Start;
-    let record = Record {activity, action, date};
+    let action_match = match action {
+        Action::Start => String::from("start"),
+        Action::Stop => String::from("stop"),
+        Action::Resume => String::from("resume"),
+    
+    };
+    let record = Record {activity, action_match, date};
     assert_eq!(record.activity, "sample task");
+    assert_eq!(record.action_match, "start");
     assert_eq!(record.date, date2);
 }
 
 struct Record {
     activity: String,
-    action: Action,
+    action_match: String,
     date: String,
 }
 
