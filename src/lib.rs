113d662638449f2d1b5ce2eda53eb41d59747e59
Paul Jickling <pjickling@gmail.com>
2019-02-11 11:25:28 -0800

    Added additional failing tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,18 @@
 fn fail_test() {
     assert_eq!(true, false);
 }
+
+#[test]
+fn write_test() {
+    assert!(false)
+}
+
+#[test]
+fn read_test() {
+    assert!(false)
+}
+
+#[test]
+fn date_test() {
+    assert!(false)
+}
