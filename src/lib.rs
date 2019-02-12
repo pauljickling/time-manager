6ed8624a0e5b5630aba44506dcde5fa7ed2dbf90
Paul Jickling <pjickling@gmail.com>
2019-02-11 16:14:07 -0800

    Relocated fns to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+use std::fs;
+use std::error::Error;
+use std::process::Command;
+
+// csv writer function for new and existing files
+pub fn _write_file(path: String, text: String) -> std::io::Result<()> {
+    fs::write(path, text)?;
+    Ok(())
+}
+
+// read file contents and return Some or None
+pub fn read(path: &String) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
+    }
+
+// gets date string from terminal
+pub fn get_date() -> String {
+    let output = Command::new("date")
+                          .output()
+                          .expect("Failed to execute command");  
+
+    let date_seq = output.stdout.as_slice();
+    let mut date = String::new();
+    for c in date_seq {
+        if c.is_ascii() == true {
+            date.push(*c as char);
+        }
+    }
+    date
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -8,17 +40,17 @@ mod tests {
     }
 
     #[test]
-    fn write_test() {
+    fn test_write_file() {
         assert!(false)
     }
 
     #[test]
-    fn read_test() {
+    fn test_read() {
         assert!(false)
     }
 
     #[test]
-    fn date_test() {
+    fn test_get_date() {
         assert!(false)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 // TODO use std::io;
 #![allow(dead_code)]
-use std::fs;
-use std::error::Error;
-use std::process::Command;
+use tm::{read, get_date};
 
 enum Action {
     Start,
@@ -41,32 +39,4 @@ struct Record {
     activity: String,
     action_match: String,
     date: String,
-}
-
-// csv writer function for new and existing files
-fn _write_file(path: String, text: String) -> std::io::Result<()> {
-    fs::write(path, text)?;
-    Ok(())
-}
-
-// read file contents and return Some or None
-fn read(path: &String) -> Result<String, Box<dyn Error>> {
-    let contents = fs::read_to_string(path)?;
-    Ok(contents)
-    }
-
-// gets date string from terminal
-fn get_date() -> String {
-    let output = Command::new("date")
-                          .output()
-                          .expect("Failed to execute command");  
-
-    let date_seq = output.stdout.as_slice();
-    let mut date = String::new();
-    for c in date_seq {
-        if c.is_ascii() == true {
-            date.push(*c as char);
-        }
-    }
-    date
 }
