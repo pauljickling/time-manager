c1247ffe4bb28633837e43aedf52b20b8704bba0
Paul Jickling <pjickling@gmail.com>
2019-02-13 15:26:53 -0800

    HashSet added to check for valid action parameter

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
+/* tm accepts the following:
+ * 1. action parameters: "start", "stop", and "resume" are valid terms.
+ * 2. activity string: used to record to csv
+ */
+
 #![allow(dead_code)]
 use std::env;
-
+use std::collections::HashSet;
 // TODO use tm::{get_date, Record, Action};
 
 fn main() {
-    if env::args().len() != 3 {
-        println!("Incorrect number of arguments.\nFormat is as follows:\ntm action activity");
+    /* Probably not the best way to implement this HashSet, but everything else I did created
+    string comparison errors. */
+    let mut actions = HashSet::new();
+    actions.insert("start".to_string());
+    actions.insert("stop".to_string());
+    actions.insert("resume".to_string());
+
+    // env parameters
+    let action_arg = env::args().nth(1);
+    let _activity_arg = env::args().nth(2);
+    
+    // TODO tm should complain if None
+    let action = match action_arg {
+        Some(x) => x.to_string(),
+        None => "none".to_string(),
+    };
+
+    if actions.contains(&action) {
+        println!("{}", action);
     } else {
-        let action = env::args().nth(1);
-        let activity = env::args().nth(2);
-        println!("{:?}\n{:?}", action.unwrap(), activity.unwrap());
+        println!("Invalid argument for action.\nstart, stop, and resume are valid arguments");
     }
+    
 } 
