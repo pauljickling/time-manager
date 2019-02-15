e209e4074f317b6ab37146f5614d9ff10edf4bb6
Paul Jickling <pjickling@gmail.com>
2019-02-15 09:02:33 -0800

    Removed Record struct and Action enum

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,6 @@ use std::fs;
 use std::error::Error;
 use std::process::Command;
 
-pub struct Record {
-    pub activity: String,
-    pub action_match: String,
-    pub date: String,
-}
-
-pub enum Action {
-    Start,
-    Stop,
-    Resume,
-}
-
 // csv writer function for new and existing files
 pub fn _write_file(path: String, text: String) -> std::io::Result<()> {
     fs::write(path, text)?;
@@ -90,21 +78,5 @@ mod tests {
     fn test_get_date() {
         let date = get_date();
         assert_eq!(date, get_date());
-    }
-    #[test]
-    fn test_struct() {
-        let activity = String::from("sample task");
-        let action = Action::Resume;
-        let date = get_date();
-        let date2 = date.clone();
-        let action_match = match action {
-            Action::Start => String::from("start"),
-            Action::Stop => String::from("stop"),
-            Action::Resume => String::from("resume"),
-        };
-        let record = Record {activity, action_match, date};
-        assert_eq!(record.activity, String::from("sample task"));
-        assert_eq!(record.action_match, "resume");
-        assert_eq!(record.date, date2);
     }
 }
