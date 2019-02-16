9e02707b90303b1b79bfcf4f3d404636ffacdb8c
Paul Jickling <pjickling@gmail.com>
2019-02-16 11:45:42 -0800

    added activity HashMap

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@
  */
 
 use std::env;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
-use tm::{get_date, get_unix_time, read, write_file};
+use tm::*;
 
 fn main() {
     let csv_path = String::from("activity_logs/sample.csv");
     let mut csv_content = read(&csv_path).unwrap();
-    println!("{}", csv_content);
+    // println!("{}", csv_content);
 
     /* Not ideal for this HashSet to be mutable, but everything else I did created
     string comparison errors. */
@@ -52,9 +52,32 @@ fn main() {
             // TODO handle calculation
             record.push_str("0\n");
             csv_content.push_str(&record);
-            println!("{}", csv_content);
+            // println!("{}", csv_content);
+            let csv_vec = parse_csv(csv_content);
+            // counter initially set to 6 as a silly hack to skip the header line
+            let mut counter = 6;
+            let mut activity_map = HashMap::new();
+            let mut current_key = String::new();
+            for i in csv_vec {
+                if counter == 11 {
+                    counter = 1;
+                }
+                if counter == 1 {
+                   current_key = i.clone();
+                }
+                if counter == 2 {
+                    activity_map.insert(current_key, i.clone());
+                    current_key = String::from("");
+                }
+                if counter == 5 {
+                    counter = 1;
+                } else {
+                    counter += 1;
+                }
+            }
+            println!("{:?}", activity_map);
             // TODO use _check_file to handle errors
-            let _check_file = write_file(csv_path, csv_content);
+            // let _check_file = write_file(csv_path, csv_content);
         }
     } else {
         println!("Invalid argument for action.\nstart, stop, and resume are valid arguments");
