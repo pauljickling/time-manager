5a403f56d46a955fba2b87e0f0b344af9243001b
Paul Jickling <pjickling@gmail.com>
2019-02-16 17:30:15 -0800

    Removed redundant if statement

--- src/main.rs
+++ src/main.rs
@@ -35,42 +35,37 @@ fn main() {
     };
 
     if actions.contains(&action) {
-        if activity == "" {
-            println!("Activity must be specified");
-        } else {
-            let record = create_record(&action, &activity);
-            csv_content.push_str(&record);
-            // println!("{}", csv_content);
-            let csv_vec = parse_csv(csv_content);
-            // counter initially set to 6 as a silly hack to skip the header line
-            let mut counter = 6;
-            let mut activity_map = HashMap::new();
-            let mut current_key = String::new();
-            for i in csv_vec {
-                if counter == 11 {
-                    counter = 1;
-                }
-                if counter == 1 {
-                   current_key = i.clone();
-                }
-                if counter == 2 {
-                    activity_map.insert(current_key, i.clone());
-                    current_key = String::from("");
-                }
-                if counter == 5 {
-                    counter = 1;
-                } else {
-                    counter += 1;
-                }
+        let record = create_record(&action, &activity);
+        csv_content.push_str(&record);
+        // println!("{}", csv_content);
+        let csv_vec = parse_csv(csv_content);
+        // counter initially set to 6 as a silly hack to skip the header line
+        let mut counter = 6;
+        let mut activity_map = HashMap::new();
+        let mut current_key = String::new();
+        for i in csv_vec {
+            if counter == 11 {
+                counter = 1;
             }
-            if Some(&action) == activity_map.get(&activity) {
-                println!("{} is already the current action for {}", action, activity);
-            } 
-            // TODO use _check_file to handle errors
-            // let _check_file = write_file(csv_path, csv_content);
+            if counter == 1 {
+               current_key = i.clone();
+            }
+            if counter == 2 {
+                activity_map.insert(current_key, i.clone());
+                current_key = String::from("");
+            }
+            if counter == 5 {
+                counter = 1;
+            } else {
+                counter += 1;
+            }
         }
+        if Some(&action) == activity_map.get(&activity) {
+            println!("{} is already the current action for {}", action, activity);
+        } 
+        // TODO use _check_file to handle errors
+        // let _check_file = write_file(csv_path, csv_content);
     } else {
         println!("Invalid argument for action.\nstart, stop, and resume are valid arguments");
     }
-    
 } 
